877fcdbc4e293147bb8b7263a809650c976dbe78
tkdlqh2 <84146296+tkdlqh2@users.noreply.github.com>
2023-03-06 09:50:35 +0900

    SOLVERTIC doc : 설명 추가

--- tkdlqh2/Solve0306.java
+++ tkdlqh2/Solve0306.java
@@ -18,6 +18,9 @@ public class Solve0306 {
 	}
 
 	public int solution(String[][] book_time) {
+		// 나가는 시간 기준으로 priority Queue 를  사용하는 유명한 greedy 문제라고 알고 있습니다.
+		// 정렬을 편하게 하기 위해 BookTime 이란 객체를 만들고
+		// 들어가는 시간 순으로 정렬을 해줍니다.
 		List<BookTime> bookTimeList= IntStream.range(0,book_time.length)
 				.mapToObj(x-> new BookTime(book_time[x]))
 				.sorted((x,y) -> {
@@ -28,6 +31,7 @@ public class Solve0306 {
 				})
 				.collect(Collectors.toList());
 
+		// 나가는 시간이 빠른 순서의 Priority Queue 를  생성해줍니다.
 		PriorityQueue<BookTime> pq = new PriorityQueue<>((x,y) -> {
 			if(y.outHour == x.outHour){
 				return x.outMinute -y.outMinute;
@@ -37,6 +41,9 @@ public class Solve0306 {
 
 		int size = 1;
 		pq.add(bookTimeList.get(0));
+
+		// 들어가는 시간 순으로 하나씩 가져오면서
+		// 들어오는 시간에 이미 나갔어야 되는 BootTime 들을  퇴장처리 해줍니다.
 		for (int i = 1; i < bookTimeList.size(); i++) {
 			BookTime curTime = bookTimeList.get(i);
 			BookTime lastTime = pq.peek();
